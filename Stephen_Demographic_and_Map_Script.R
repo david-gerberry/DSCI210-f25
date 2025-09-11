1ccad7a1dd76cf0b5bd13a19e21940ac444dcfc8
Stephen Lynch <stephenbenlynch@gmail.com>
2025-09-11 00:56:54 -0400

    lwl a;rfeialuabelf

--- Stephen_Demographic_and_Map_Script.R
+++ Stephen_Demographic_and_Map_Script.R
@@ -2,8 +2,93 @@ library(tidyverse)
 library(tidycensus)
 library(ggplot2)
 library(sf)
+library(dplyr)
 
 
+
+#### voter data ####
+
+voter_df <- read_csv("data/VoterListExport-20250911-CICINC-CI-pn.csv")
+
+voter_df_clean <- voter_df %>% select(-5:-31)
+  
+write.csv(voter_df_clean, "data/voter_df_clean.csv")
+
+voter_df <- read_csv("data/voter_df_clean.csv")
+
+
+count_a_u <- "A-U"
+
+voter_df$num_absnt_un <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_a_u, na.rm = TRUE)
+})
+
+count_p_u <- "P-U"
+
+voter_df$num_polls_un <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_p_u, na.rm = TRUE)
+})
+
+count_p_d <- "P-D"
+
+voter_df$num_polls_d <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_p_d, na.rm = TRUE)
+})
+
+count_p_r <- "P-R"
+
+voter_df$num_polls_r <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_p_r, na.rm = TRUE)
+})
+
+count_a_d <- "A-D"
+
+voter_df$num_absnt_d <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_a_d, na.rm = TRUE)
+})
+
+count_a_r <- "A-R"
+
+voter_df$num_absnt_r <- apply(voter_df[, 6:ncol(voter_df)], 1, function(row) {
+  sum(row == count_a_r, na.rm = TRUE)
+})
+
+
+stuff_by_presinct <-voter_df %>%
+  group_by(PrecinctNum)%>%
+  summarise(sum_polls_un = sum(num_polls_un, na.rm = TRUE),
+            sum_polls_d = sum(num_polls_d, na.rm = TRUE),
+            sum_polls_r = sum(num_polls_r, na.rm = TRUE),
+            sum_absnt_d = sum(num_absnt_d, na.rm = TRUE),
+            sum_absnt_r = sum(num_absnt_r, na.rm = TRUE),
+            sum_absnt_un = sum(num_absnt_un, na.rm = TRUE))
+
+write.csv(stuff_by_presinct, "data/sum_by_precinct.csv")
+
+summarized_data <- read_csv("data/sum_by_precinct.csv")
+
+
+summarized_data_long <- summarized_data %>%
+  pivot_longer(
+    cols = starts_with("sum_"),
+    names_to = "Category",
+    values_to = "Count"
+  )
+
+
+ggplot(summarized_data_long, aes(x = Category, y = Count, fill = Category)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~PrecinctNum, scales = "free_y") +
+  labs(
+    title = "Voting Status Frequency by Precinct",
+    x = "Voting Category",
+    y = "Frequency"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none"
+  )
 
 #### Pulling Census Data ####
 
