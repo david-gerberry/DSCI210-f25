5b6050bc097db144f45dfc988d0665be38f1dada
NickMallow <nick.mallow15@gmail.com>
2025-10-21 14:34:52 -0400

    the "J"

--- NickBase Swing maps.R
+++ NickBase Swing maps.R
@@ -4,6 +4,53 @@ ThresholdStrong = magic_number_average * 2
 ThresholdsSwing = magic_number_average * .8
 max_support = max(AvgMap$AvgProx, na.rm = T)
 
+leaf_mapN = function() {
+  pal <- colorFactor(
+    palette = c(
+      "Residual" = "red",
+      "Swing" = "gold",
+      "Base" = "blue",
+      "Strong Base" = "darkblue"
+    ),
+    levels = c("Residual", "Swing", "Base", "Strong Base")
+  )
+  
+  # Create the map
+  AvgMap %>%
+    mutate(BaseSwing = cut(
+      AvgProx,
+      breaks = c(-0.001, ThresholdsSwing, ThresholdsRes, ThresholdStrong, 1),
+      labels = c('Residual', 'Swing', 'Base', 'Strong Base')
+    )) %>%
+    st_transform(4326) %>%  # Transform to WGS84 for leaflet
+    leaflet() %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%  # Add a base map
+    addPolygons(
+      fillColor = ~pal(BaseSwing),
+      fillOpacity = 0.7,
+      color = "white",
+      weight = 1,
+      label = ~BaseSwing,
+      highlightOptions = highlightOptions(
+        weight = 2,
+        color = "#666",
+        fillOpacity = 0.9,
+        bringToFront = TRUE
+      )
+    ) %>%
+    addLegend(
+      position = "bottomright",
+      pal = pal,
+      values = ~BaseSwing,
+      title = "BaseSwing",
+      opacity = 0.7
+    )
+}
+
+
+
+
+save(AvgMap, ThresholdsSwing, ThresholdStrong, ThresholdsRes, leaf_mapN, file = "data/Elec.RData")
 
 SmallMap %>% 
   ggplot(aes(fill = PrecentPorxy)) +
